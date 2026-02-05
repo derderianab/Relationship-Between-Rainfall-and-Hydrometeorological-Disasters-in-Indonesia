54e821c0d80b4ef694590a653bc8a69a6cdd3dad
Der Derian Auliyaa Bainus <derderianab@gmail.com>
2026-02-05 11:42:12 +0700

    Update main.py
    changed regency_boundaries input
    joined disaster data and polygon
    join precipitation value to disaster data
    tested 2025 data

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ import rioxarray
 from rasterstats import zonal_stats
 
 
+def flip_raster_orientation(precip):
+    if precip.latitude.values[0] < precip.latitude.values[-1]:
+        flipped_raster = precip.sortby("latitude", ascending=False)
+        return flipped_raster
+    return precip
+
+
 ### Datasets
 disaster_data = r"data/2021_2025_disaster.csv"
-regency_boundaries = r"data/RBI50K_ADMINISTRASI_KABKOTA_20230907.gdb"
+regency_boundaries = r"data/shapefile/regency_boundaries.shp"
 rainfall_p05_2025 = r"data/rainfall/chirps-v2.0.2025.days_p05.nc"
 rainfall_p25_2025 = r"data/rainfall/chirps-v2.0.2025.days_p25.nc"
 # Indonesia's Extend
@@ -19,28 +26,60 @@ Long = [94, 142]
 ##### Pre Process Disaster Data
 disaster_df = pd.read_csv(disaster_data)
 wet_hidromet_class = ["Banjir", "Cuaca ekstrem", "Longsor", "Gelombang pasang / Abrasi"]
-wet_hidromet_df = disaster_df[disaster_df["Jenis Bencana"].isin(wet_hidromet_class)]
-print(wet_hidromet_df['Tanggal / Waktu Kejadian'])
+hidromet_df = disaster_df[disaster_df["Jenis Bencana"].isin(wet_hidromet_class)].copy()
+hidromet_df["Kode Kabupaten"] = hidromet_df["Kode Kabupaten"].astype(str)
 
 
 ##### Pre Process Regencies Data
-regencies = gpd.read_file(regency_boundaries, layer="ADMINISTRASI_AR_KABKOTA", columns=["KDPKAB", "NAMOBJ"])
-regencies = regencies.to_crs("EPSG:4326")
-test_regency = regencies.iloc[0]
+regencies_gdf = gpd.read_file(regency_boundaries, columns=["KDPKAB", "NAMOBJ"])
+regencies_gdf = regencies_gdf.to_crs("EPSG:4326")
+#regencies_gdf["KDPKAB"] = regencies_gdf["KDPKAB"].astype(float)
+test_regency = regencies_gdf.iloc[0]
 test_polygon = test_regency.geometry
-#print(test_polygon)
 
+
+##### Join Polygon to Disaster Data Frame
+hidromet_df_with_geom = hidromet_df.merge(regencies_gdf[["KDPKAB", "geometry"]], left_on="Kode Kabupaten", right_on="KDPKAB", how="left")
+hidromet_gdf=gpd.GeoDataFrame(hidromet_df_with_geom, geometry="geometry", crs=regencies_gdf.crs)
+hidromet_gdf[["date", "time"]] = hidromet_gdf["Tanggal / Waktu Kejadian"].str.split(' ', expand=True)
+hidromet_gdf["date"] = pd.to_datetime(hidromet_gdf["date"])
+hidromet_gdf["precip"] = None
+print(hidromet_gdf[["Tanggal / Waktu Kejadian", "date", "time"]])
+print(hidromet_gdf.columns)
+print(hidromet_gdf.iloc[0].Tahun)
+print(len(hidromet_gdf))
 
 ##### Pre Process Rainfall Data
-ds = xr.open_dataset(rainfall_p25_2025)
-precip = ds.precip
+ds_2025 = xr.open_dataset(rainfall_p25_2025)
+precip_2025 = ds_2025.precip
+precip_2025 = flip_raster_orientation(precip_2025)
 
-if precip.latitude.values[0] < precip.latitude.values[-1]:
-    precip = precip.sortby("latitude", ascending=False)
 
-print(precip.time)
+#### Add Daily Precipitation Value to Disaster
+for i in range(len(hidromet_gdf)):
+    if hidromet_gdf.iloc[i].Tahun == 2025:
 
-### Overlay NC and Polygon
+        geom = hidromet_gdf.iloc[i].geometry
+        if geom is None:
+            hidromet_gdf.loc[hidromet_gdf.index[i], "precip"] = None
+            continue
+        daily = precip_2025.sel(time=hidromet_gdf.iloc[i].date)
+        stats = zonal_stats(
+            geom,
+            daily.values,
+            affine=daily.rio.transform(),
+            stats="mean",
+            nodata=-9999
+        )
+        hidromet_gdf.loc[hidromet_gdf.index[i], "precip"] = stats[0]["mean"]
+
+        print(stats)
+        print(hidromet_gdf.iloc[i]["precip"])
+
+#Output to Shapefile
+hidromet_gdf.to_file("output/test_2025.shp")
+
+#### Overlay NC and Polygon
 # results = []
 # for t in range(len(precip.time)):
 #     daily = precip.isel(time=t)
