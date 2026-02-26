1a0643427bade626702a20c480ed3111d3dd9777
Der Derian Auliyaa Bainus <derderianab@gmail.com>
2026-02-26 11:29:39 +0700

    update main
    use test data
    groupby and aggregate to calculate precip mean value

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def flip_raster_orientation(precip):
 
 ### Datasets
 disaster_data = r"data/2021_2025_disaster.csv"
+test_data = r"data/test_data.csv"
 regency_boundaries = r"data/shapefile/regency_boundaries.shp"
 rainfall_p05_2025 = r"data/rainfall/chirps-v2.0.2025.days_p05.nc"
 rainfall_p25_2025 = r"data/rainfall/chirps-v2.0.2025.days_p25.nc"
@@ -24,8 +25,8 @@ Long = [94, 142]
 
 
 ##### Pre Process Disaster Data
-disaster_df = pd.read_csv(disaster_data)
-wet_hidromet_class = ["Banjir", "Cuaca ekstrem", "Longsor", "Gelombang pasang / Abrasi"]
+disaster_df = pd.read_csv(test_data)
+wet_hidromet_class = ["Banjir", "Longsor"]
 hidromet_df = disaster_df[disaster_df["Jenis Bencana"].isin(wet_hidromet_class)].copy()
 hidromet_df["Kode Kabupaten"] = hidromet_df["Kode Kabupaten"].astype(str)
 
@@ -33,9 +34,6 @@ hidromet_df["Kode Kabupaten"] = hidromet_df["Kode Kabupaten"].astype(str)
 ##### Pre Process Regencies Data
 regencies_gdf = gpd.read_file(regency_boundaries, columns=["KDPKAB", "NAMOBJ"])
 regencies_gdf = regencies_gdf.to_crs("EPSG:4326")
-#regencies_gdf["KDPKAB"] = regencies_gdf["KDPKAB"].astype(float)
-test_regency = regencies_gdf.iloc[0]
-test_polygon = test_regency.geometry
 
 
 ##### Join Polygon to Disaster Data Frame
@@ -44,18 +42,16 @@ hidromet_gdf=gpd.GeoDataFrame(hidromet_df_with_geom, geometry="geometry", crs=re
 hidromet_gdf[["date", "time"]] = hidromet_gdf["Tanggal / Waktu Kejadian"].str.split(' ', expand=True)
 hidromet_gdf["date"] = pd.to_datetime(hidromet_gdf["date"])
 hidromet_gdf["precip"] = None
-print(hidromet_gdf[["Tanggal / Waktu Kejadian", "date", "time"]])
 print(hidromet_gdf.columns)
-print(hidromet_gdf.iloc[0].Tahun)
-print(len(hidromet_gdf))
+
 
 ##### Pre Process Rainfall Data
 ds_2025 = xr.open_dataset(rainfall_p25_2025)
-precip_2025 = ds_2025.precip
-precip_2025 = flip_raster_orientation(precip_2025)
+raw_precip_2025 = ds_2025.precip
+precip_2025 = flip_raster_orientation(raw_precip_2025)
 
 
-#### Add Daily Precipitation Value to Disaster
+#### Add Daily Precipitation Value to Disaster Dataset
 for i in range(len(hidromet_gdf)):
     if hidromet_gdf.iloc[i].Tahun == 2025:
 
@@ -69,15 +65,40 @@ for i in range(len(hidromet_gdf)):
             daily.values,
             affine=daily.rio.transform(),
             stats="mean",
-            nodata=-9999
+            nodata=-9999,
+            all_touched=True
         )
         hidromet_gdf.loc[hidromet_gdf.index[i], "precip"] = stats[0]["mean"]
 
         print(stats)
         print(hidromet_gdf.iloc[i]["precip"])
 
-#Output to Shapefile
-hidromet_gdf.to_file("output/test_2025.shp")
+
+### Calculate Precip Mean Value of Each Regency
+grouped_gdf = hidromet_gdf.groupby(by="Kode Kabupaten")
+kab_stats = (
+    hidromet_gdf
+    .groupby("KDPKAB")
+    .agg(
+        nama_kab=("Kabupaten", "first"),
+        geometry=("geometry", "first"),
+        mean_precip=("precip", "mean"),
+        max_precip=("precip", "max"),
+        min_precip=("precip", "min"),
+        median_precip=("precip", "median"),
+        jumlah_kejadian=("precip", "count")
+    )
+    .reset_index()
+)
+
+kab_stats = gpd.GeoDataFrame(kab_stats, geometry="geometry", crs=hidromet_gdf.crs)
+print(kab_stats)
+
+#Function parameters
+##hidromet_gdf
+##Tahun
+
+
 
 #### Overlay NC and Polygon
 # results = []
